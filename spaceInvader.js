f56625fa4077c21acaff4289b64ea70a091df993
aliasaad22 <ali.asaad1@student.ehb.be>
2024-09-30 15:21:15 +0200

    opdracht Signature

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,23 @@
-"use strict";
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
+signature();
+function signature() {
+	context.fillStyle = "black";
+	context.fillRect(25, 25, 350, 350);
+	context.fillStyle = "#D07B6D";
+	context.fillRect(100, 50, 200, 200);
 
-alert("Draw your space invader here");
+	context.fillStyle = "white";
+	context.fillRect(165, 115, 70, 70);
+	context.fillStyle = "#D07B6D";
+	context.fillRect(50, 250, 50, 50);
+	context.fillRect(50, 300, 50, 50);
+	context.fillRect(100, 300, 50, 50);
+	context.fillRect(150, 300, 50, 50);
+	context.fillRect(200, 300, 50, 50);
+	context.fillRect(250, 300, 50, 50);
+	context.fillRect(300, 300, 50, 50);
+	context.fillRect(300, 250, 50, 50);
+}
